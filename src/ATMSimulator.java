f3bbc3a10611ed4a604575b070a444a4ccb10ebf
Tinashe <tinashezvandasara@gmail.com>
2026-09-14 17:45:12 +0200

    Added arrays for the accounts, PINS and amounts. Added a clean showBalance() display, still tweaking deposit and withdraw Need to add receipts and transaction history

--- src/ATMSimulator.java
+++ src/ATMSimulator.java
@@ -4,16 +4,27 @@ import java.text.*;
 
 public class ATMSimulator
 {
-     static void main(String args[])
+    // ACCOUNT DATA
+    static String[] accountNumbers = {"8245", "1234", "5678"};
+    static String[] accountPins = {"0000", "1111", "2222"};
+    static double[] balances = {500000.00, 25000.00, 7500.00};
+    static String[] ownerNames = {"John Doe", "Jane Smith", "Bob Marley"};
+
+    static void main(String args[])
     {
         // Variables
         int selection;
         int attempts = 0;
-        String accountNumber = "8245";
-        String pin = "0000";
-        double balance = 500000;
+        int accountIndex = -1;
         boolean running = true;
         boolean loggedIn = false;
+        double dailyWithdrawal = 0.00;
+
+
+
+        //Constants
+        final double MINIMUM_BALANCE = 1000.00;
+        final double DAILY_LIMIT = 10000.00;
 
 
         //ErrorTypes errorTypes = Error;   /enums
@@ -37,26 +48,54 @@ public class ATMSimulator
                     System.out.println("Enter your pin: ");
                     String enteredPin = sc.nextLine().trim();
 
-                    if(accountEntered.equals(accountNumber) && enteredPin.equals(pin))
+                    int foundIndex = -1;
+
+                    for(int i = 0; i < accountNumbers.length;i++)
                     {
-                        loggedIn = true;
+                        if(accountNumbers[i].equals(accountEntered))
+                        {
+                            foundIndex = i;
+                            break;
+                        }
+                    }
 
-                        System.out.println("Logged in successfully, Welcome! \n");
-
-                    } else {
+                    if(foundIndex == -1)
+                    {
                         attempts++;
+
+                        showError(ATMError.ACCOUNT_NOT_FOUND);
 
                         int remaining = 3 - attempts;
 
-                        System.out.println("Invalid credentials." + (remaining > 0 ? " Attempts remaining: " + remaining + "\n" : ""));
+                        System.out.println(" Invalid Account Number. " + (remaining > 0 ? " Attempts remaining: " + remaining + "\n" : ""));
+
+                    }
+                    else if(enteredPin.equals(accountPins[foundIndex]))
+                    {
+                        loggedIn = true;
+
+                        accountIndex = foundIndex;
+
+                        System.out.println("\nLogin successful. Welcome, " + ownerNames[accountIndex] + "!\n");
+
+                    }
+                    //For Wrong PIN
+                    else
+                    {
+                        attempts++;
+
+                        showError(ATMError.INVALID_CREDENTIALS);
+
+                        int remaining = 3 - attempts;
+
+                        System.out.println("Invalid PIN. " + (remaining > 0 ? " Attempts remaining: " + remaining + "\n" : ""));
                     }
 
                     if(attempts == 3)
                     {
                         showError(ATMError.ACCOUNT_BLOCKED);
 
-                        running = exit();
-
+                        running = exit(sc);
                     }
 
                 }
@@ -86,13 +125,13 @@ public class ATMSimulator
 
                     switch(selection)
                     {
-                        case 1 -> showBalance(balance);
+                        case 1 -> showBalance(accountIndex);
 
-                        case 2 -> balance = deposit(sc, balance);
+                        case 2 -> balances[accountIndex] = deposit(sc, accountIndex);
 
-                        case 3 -> balance = withdraw(sc, balance);
+                        case 3 -> balances[accountIndex] = withdraw(sc, accountIndex, dailyWithdrawal);
 
-                        case 4 -> running = exit();
+                        case 4 -> running = exit(sc);
 
                         //case 5 ->
 
@@ -109,7 +148,7 @@ public class ATMSimulator
                 {
                     System.out.println("Input error. Ending session...");
 
-                    running = exit();
+                    running = exit(sc);
                 }
             }
 
@@ -121,65 +160,89 @@ public class ATMSimulator
 
     //FUNCTIONS
 
-    static void showBalance(double balance)
+    static void showBalance(int index)
     {
-        FormattedBalance(balance);
+        System.out.println("\n--- ACCOUNT BALANCE ---------");
+        System.out.printf("\nOwner: %s" , ownerNames[index]);
+        System.out.printf("\nAccount: %s\n", accountNumbers[index]);
+
+        FormattedBalance(balances[index]);
+
+        System.out.println("-----------------------------\n");
 
     }
 
-    static double deposit(Scanner sc, double balance)
+    static double deposit(Scanner sc, int index)
     {
         System.out.print("How much would you like to deposit?");
 
         double deposit = getValidAmount(sc);
 
-        balance += deposit;
+        balances[index] += deposit;
 
-        FormattedBalance(balance);
+        FormattedBalance(balances[index]);
 
-        return balance;
+        return balances[index];
     }
 
-    static double withdraw(Scanner sc, double balance)
+
+    static double withdraw(Scanner sc, int index, double dailyWithdrawal)
     {
+        System.out.println("Withdrawals occurs in multiples of $50: ");
+
         System.out.print("How much would you like to withdraw?");
 
         double withdraw = getValidAmount(sc);
 
-        System.out.println("Withdrawals occurs in multiples of $50: ");
 
         if(withdraw % 50 != 0)
         {
-            System.out.println("Withdrawals must be in multiples of $50. ");
+            showError(ATMError.INVALID_DENOMINATION);
 
-            FormattedBalance(balance);
-
-            return balance;
+           // return new double[] {balances[index], dailyWithdraw};
         }
 
 
-
-
-        if(withdraw > balance )
+        if(withdraw > balances[index] )
         {
-            System.out.println("You are trying to withdraw an amount exceeding your balance");
+           showError(ATMError.INSUFFICIENT_FUNDS);
 
-            FormattedBalance(balance);
+            FormattedBalance(balances[index]);
 
-            return balance;
+            return balances[index];
 
         }else
         {
-            balance -= withdraw;
+            balances[index] -= withdraw;
+
+            // dailyWithdrawal += withdraw;
+
+           /* if(dailyWithdrawal == DAILY_LIMIT)
+            {
+                System.out.println("You have exceeded your daily withdrawal limit. Please choose another option.");
+
+                FormattedBalance(balances[index]);
+
+                return balances[index];
+            }
+
+            if(balances[index] == MINIMUM_BALANCE)
+            {
+                System.out.println("Your balance is low, please deposit.");
+
+                FormattedBalance(balances[index]);
+
+                return balances[index];
+            } */
 
         }
 
-        FormattedBalance(balance);
+        FormattedBalance(balances[index]);
 
-        return balance;
+        return balances[index];
     }
 
-    static boolean exit()
+    static boolean exit(Scanner sc)
     {
         System.out.println("Thank you for using the ATM");
 
